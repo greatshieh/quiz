68f10ec927df9b2e25b928b718e8535956513fc9
greatshieh <ivan.xiewei@gmail.com>
2018-07-12 18:46:29 +0800

    题目的简单显示。点击下一一题

--- client/pages/person/person.js
+++ client/pages/person/person.js
@@ -1,14 +1,5 @@
 const qcloud = require('../../vendor/wafer2-client-sdk/index')
-
 const config = require('../../config.js')
-
-
-var touchDot = 0; //触摸时的原点
-var time = 0; //  时间记录，用于滑动时且时间小于1s则执行左右滑动
-var interval = ""; // 记录/清理 时间记录
-var nth = 0; // 设置活动菜单的index
-var nthMax = 20; //活动菜单的最大个数
-var tmpFlag = true; // 判断左右华东超出菜单最大值时不再执行滑动事件
 
 Page({
 
@@ -17,36 +8,11 @@ Page({
    */
   data: {
     title: '',
-    option: []
+    option: [],
+    title_cnt: 0, //题目计数
+    lFlag: false
   },
-  // 触摸开始事件
-  touchStart: function(e) {
-    touchDot = e.touches[0].pageX; // 获取触摸时的原点
-    // 使用js计时器记录时间    
-    interval = setInterval(function() {
-      time++;
-    }, 100);
-  },
-  // 触摸移动事件
-  touchMove: function(e) {
-    var touchMove = e.touches[0].pageX;
-    
-    // 向左滑动   
-    if (touchMove - touchDot <= -40 && time < 10) {
-      console.log("左滑")
-      if (tmpFlag && nth < nthMax) { //每次移动中且滑动时不超过最大值 只执行一次
-        this.getQuiz(); // 更新题目,
-        nth += 1
-      }
-    }
-  },
-  // 触摸结束事件
-  touchEnd: function(e) {
-    clearInterval(interval); // 清除setInterval
-    time = 0;
-    tmpFlag = true; // 回复滑动事件
-  },
-
+  
   getQuiz() {
     wx.showLoading({
       title: '加载题目',
@@ -64,18 +30,23 @@ Page({
 
         var options = temp.split(",")
 
-        console.log(temp)
+        var cnt = this.data.title_cnt
+        cnt += 1
         this.setData({
           title: data.title,
-          option: options
+          option: options,
+          title_cnt: cnt,
+          lFlag: true
         })
       },
 
       fail: () => {
         wx.hideLoading()
-
         wx.showToast({
           title: '题目列表加载错误',
+        })
+        this.setDataP({
+          lFlag: true,
         })
       }
     })
@@ -100,7 +71,6 @@ Page({
    * 生命周期函数--监听页面显示
    */
   onShow: function() {
-
   },
 
   /**
