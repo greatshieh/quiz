4cd5527745364e12dd48ee8a2533d2583570431b
greatshieh <ivan.xiewei@gmail.com>
2018-07-23 10:00:02 +0800

    优化登录

--- client/app.js
+++ client/app.js
@@ -1,6 +1,7 @@
 //app.js
 var qcloud = require('./vendor/wafer2-client-sdk/index')
 var config = require('./config')
+let userInfo
 
 App({
   data: {
@@ -8,5 +9,91 @@ App({
   },
   onLaunch: function() {
     qcloud.setLoginUrl(config.service.loginUrl)
-  }
+  },
+
+  checkSession({
+    success,
+    error
+  }) {
+    wx.checkSession({
+      success: () => {
+        this.getUserInfo({
+          success,
+          error
+        })
+      },
+      fail: () => {
+        console.log("session-key无效")
+        wx.showModal({
+          title: '提示',
+          content: '请授权我们获取您的用户信息，以便提供详细的成绩分析报告',
+          success: (res) => {
+            if (res.confirm) {
+              console.log("授权登录")
+              this.doQcloudLogin({
+                success,
+                error
+              })
+            } else {
+              console.log("没有授权")
+            }
+          }
+        })
+        error && error()
+      }
+    })
+  },
+
+  getUserInfo({
+    success,
+    error
+  }) {
+    qcloud.request({
+      url: config.service.requestUrl,
+      login: true,
+      success: result => {
+        let data = result.data
+
+        if (!data.code) {
+          let userInfo = data.data
+
+          success && success({
+            userInfo
+          })
+        } else {
+          error && error()
+        }
+      },
+      fail: () => {
+        error && error()
+      }
+    })
+  },
+
+  doQcloudLogin({
+    success,
+    error
+  }) {
+    // 调用 qcloud 登陆接口
+    qcloud.login({
+      success: result => {
+        if (result) {
+          let userInfo = result
+          console.log(userInfo)
+          success && success({
+            userInfo
+          })
+        } else {
+          // 如果不是首次登录，不会返回用户信息，请求用户信息接口获取
+          this.getUserInfo({
+            success,
+            error
+          })
+        }
+      },
+      fail: () => {
+        error && error()
+      }
+    })
+  },
 })
--- client/pages/home/home.js
+++ client/pages/home/home.js
@@ -2,6 +2,8 @@
 var qcloud = require('../../vendor/wafer2-client-sdk/index')
 var config = require('../../config')
 
+const app = getApp()
+
 Page({
   data: {
     userInfo: null,
@@ -11,7 +13,11 @@ Page({
 
   // 获取用户信息
   onLoad: function(options) {
-    this.checkSession({
+
+  },
+
+  onShow: function() {
+    app.checkSession({
       success: ({
         userInfo
       }) => {
@@ -27,40 +33,14 @@ Page({
         })
       },
       error: () => {
-        console.log("session-key无效")
-        wx.showModal({
-          title: '没有登录',
-          content: '',
-        })
       }
     })
   },
 
-  checkSession({
-    success,
-    error
-  }) {
-    wx.checkSession({
-      success: () => {
-        this.getUserInfo({
-          success,
-          error
-        })
-      },
-      fail: () => {
-        error && error()
-      }
-    })
-  },
 
   onTapLogin: function(e) {
-    // this.setData({
-    //   userInfo: e.detail.userInfo,
-    //   logged: true
-    // })
-
     // 保存到用户信息到缓存
-    this.doQcloudLogin({
+    app.doQcloudLogin({
       success: ({
         userInfo
       }) => {
@@ -73,59 +53,6 @@ Page({
     wx.setStorage({
       key: 'userinfo',
       data: e.detail.userInfo,
-    })
-  },
-
-  getUserInfo({
-    success,
-    error
-  }) {
-    qcloud.request({
-      url: config.service.requestUrl,
-      login: true,
-      success: result => {
-        let data = result.data
-
-        if (!data.code) {
-          let userInfo = data.data
-
-          success && success({
-            userInfo
-          })
-        } else {
-          error && error()
-        }
-      },
-      fail: () => {
-        error && error()
-      }
-    })
-  },
-
-  doQcloudLogin({
-    success,
-    error
-  }) {
-    // 调用 qcloud 登陆接口
-    qcloud.login({
-      success: result => {
-        if (result) {
-          let userInfo = result
-          console.log(userInfo)
-          success && success({
-            userInfo
-          })
-        } else {
-          // 如果不是首次登录，不会返回用户信息，请求用户信息接口获取
-          this.getUserInfo({
-            success,
-            error
-          })
-        }
-      },
-      fail: () => {
-        error && error()
-      }
     })
   },
 })
